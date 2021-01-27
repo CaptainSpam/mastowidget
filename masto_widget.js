38cec5bc3206bffb77562f9e00730b64cd8dfb09
Nicholas Killewald <captainspam@gmail.com>
2021-01-27 00:17:14 -0800

    Standardized bracing style.
    Yeah, let's just do this the way I usually do it.

--- masto_widget.js
+++ masto_widget.js
@@ -32,13 +32,11 @@ const longLoadingText = "Loading (in theory)...";
 var longLoadingElem;
 const longLoadingDelay = 5000;
 
-function longLoadingMessage()
-{
+function longLoadingMessage() {
     longLoadingElem.text(longLoadingText);
 }
 
-function constructHtml(base)
-{
+function constructHtml(base) {
     // This just builds up the HTML such that we only need one div on the page
     // to begin with.
     base.empty();
@@ -106,8 +104,7 @@ function fetchAccountData() {
     // we're just going to grab it now so we know what the user is.  What's
     // returned with individual statuses might be things like the original
     // author of a boosted status or whatnot.
-    $.get(accountUrl, "", function(data, textStatus, jqXHR)
-    {
+    $.get(accountUrl, "", function(data, textStatus, jqXHR) {
         // Here comes author data!  And it's in nifty JSON format, too!
         authorData = extractAuthorDataFromJson(data);
 
@@ -117,8 +114,7 @@ function fetchAccountData() {
 
 function fetchStatuses() {
     // Status time!
-    $.get(statusesUrl, "", function(data, textStatus, jqXHR)
-    {
+    $.get(statusesUrl, "", function(data, textStatus, jqXHR) {
         // Post data should just be an array of, well, post data.
         postData = data;
 
@@ -143,50 +139,41 @@ function extractAuthorDataFromJson(json) {
     return authorData;
 }
 
-function showError(errorText)
-{
+function showError(errorText) {
     const base = $(baseDiv);
     setMode(base, "error");
     const error = base.find(".mw_error");
     error.text(errorText);
 }
 
-function genericFetchError(data)
-{
+function genericFetchError(data) {
     // Chances are the browser already dumped an error to console.log in this
     // case, so we don't need to do that here.
     showError("There was some sort of problem reading your data.  If you're sure you typed it in right, maybe that server doesn't allow cross-domain Javascript widgets access to the feed (Mastodon instances in particular might deny access by default)?");
 }
 
-function setMode(base, modeString)
-{
+function setMode(base, modeString) {
     // Our modes of choice today are:
     //
     // "loading"
     // "display"
     // "error"
-    if(modeString === "loading")
-    {
+    if(modeString === "loading") {
         base.find(".mw_loading").toggle(true);
         base.find(".mw_mainblock").toggle(false);
         base.find(".mw_error").toggle(false);
-    }
-    else if(modeString === "display")
-    {
+    } else if(modeString === "display") {
         base.find(".mw_loading").toggle(false);
         base.find(".mw_mainblock").toggle(true);
         base.find(".mw_error").toggle(false);
-    }
-    else if(modeString === "error")
-    {
+    } else if(modeString === "error") {
         base.find(".mw_loading").toggle(false);
         base.find(".mw_mainblock").toggle(false);
         base.find(".mw_error").toggle(true);
     }
 }
 
-function showAuthorData(base)
-{
+function showAuthorData(base) {
     base.find(".mw_avatar").parent().attr("href", authorData["uri"]);
     base.find(".mw_avatar").css("background-image", "url(\"" + authorData["avatar"] + "\")");
 
@@ -212,8 +199,7 @@ function showAuthorData(base)
     }
 }
 
-function showAllPosts(base)
-{
+function showAllPosts(base) {
     var entries = base.find(".mw_contentblock");
 
     // Later, we'll want to be able to update the content (i.e. adding more
@@ -221,83 +207,78 @@ function showAllPosts(base)
     // now, let's always assume a complete wipe.
     entries.empty();
 
-    $.each(postData, function(index, data)
-            {
-                var entryElem = $(document.createElement("div"));
-                entryElem.addClass("mw_entry");
-                entryElem.attr("id", data["id"]);
+    $.each(postData, function(index, data) {
+        var entryElem = $(document.createElement("div"));
+        entryElem.addClass("mw_entry");
+        entryElem.attr("id", data["id"]);
 
-                var curElem;
+        var curElem;
 
-                // The current layout is pretty simple.  Should just be a series
-                // of blocks stacked on top of each other.
+        // The current layout is pretty simple.  Should just be a series of
+        // blocks stacked on top of each other.
 
-                // First, the date.  I'll do more conversion later, but for now,
-                // the string we got for the date should be convertable to the
-                // local timezone this way.  There's more stable ways to do
-                // this, I know.
-                var date = new Date(data["created_at"]);
-                curElem = $(document.createElement("div"));
-                curElem.addClass("mw_entry_date");
+        // First, the date.  I'll do more conversion later, but for now, the
+        // string we got for the date should be convertable to the local
+        // timezone this way.  There's more stable ways to do this, I know.
+        var date = new Date(data["created_at"]);
+        curElem = $(document.createElement("div"));
+        curElem.addClass("mw_entry_date");
 
-                var aElem = $(document.createElement("a"));
-                aElem.attr("href", data["url"]);
-                aElem.attr("target", baseTarget);
-                aElem.attr("rel", "noopener");
-                aElem.text(date);
-                curElem.append(aElem);
-                entryElem.append(curElem);
+        var aElem = $(document.createElement("a"));
+        aElem.attr("href", data["url"]);
+        aElem.attr("target", baseTarget);
+        aElem.attr("rel", "noopener");
+        aElem.text(date);
+        curElem.append(aElem);
+        entryElem.append(curElem);
 
-                // TODO: This isn't how in-reply-to works, fix this.
-                if("in-reply-to" in data)
-                {
-                    curElem = $(document.createElement("div"));
-                    curElem.addClass("mw_in_reply_to");
+        // TODO: This isn't how in-reply-to works, fix this.
+        if("in-reply-to" in data) {
+            curElem = $(document.createElement("div"));
+            curElem.addClass("mw_in_reply_to");
 
-                    aElem = $(document.createElement("a"));
-                    aElem.attr("href", data["conversation"]);
-                    aElem.attr("target", baseTarget);
-                    aElem.attr("rel", "noopener");
-                    aElem.text("(part of a conversation)");
-                    curElem.append(aElem);
-                    entryElem.append(curElem);
-                }
+            aElem = $(document.createElement("a"));
+            aElem.attr("href", data["conversation"]);
+            aElem.attr("target", baseTarget);
+            aElem.attr("rel", "noopener");
+            aElem.text("(part of a conversation)");
+            curElem.append(aElem);
+            entryElem.append(curElem);
+        }
 
-                // Get the content and paste that in, too.  This may need more
-                // careful analysis later; as it stands, content comes in as
-                // HTML, and I have to dump that in to make it look right.  To
-                // that end, though, this needs to be sanitized properly.  I
-                // would think Mastodon would sanitize things on their side, but
-                // hey, never can be too sure, right?
-                //
-                // TODO: Make said sanitizing function, do the same with author
-                // summary.  Just removing <script> tags isn't good enough; we
-                // really should remove any of the on* attributes, any attribute
-                // whose value starts with "javascript:", etc.
-                curElem = $(document.createElement("div"));
-                curElem.addClass("mw_entry_content");
-                var content = $(data["content"]);
-                content.find("script").remove();
-                curElem.append(content);
+        // Get the content and paste that in, too.  This may need more careful
+        // analysis later; as it stands, content comes in as HTML, and I have to
+        // dump that in to make it look right.  To that end, though, this needs
+        // to be sanitized properly.  I would think Mastodon would sanitize
+        // things on their side, but hey, never can be too sure, right?
+        //
+        // TODO: Make said sanitizing function, do the same with author summary.
+        // Just removing <script> tags isn't good enough; we really should
+        // remove any of the on* attributes, any attribute whose value starts
+        // with "javascript:", etc.
+        curElem = $(document.createElement("div"));
+        curElem.addClass("mw_entry_content");
+        var content = $(data["content"]);
+        content.find("script").remove();
+        curElem.append(content);
 
-                // Unlike the RSS version, it looks like Mastodon already takes
-                // care of rel and target="_blank" stuff.  That's handy.
+        // Unlike the RSS version, it looks like Mastodon already takes care of
+        // rel and target="_blank" stuff.  That's handy.
 
-                entryElem.append(curElem);
+        entryElem.append(curElem);
 
-                // Finally, toss the block on to the end!
-                entries.append(entryElem);
+        // Finally, toss the block on to the end!
+        entries.append(entryElem);
 
-                // And add a separator.
-                entries.append($(document.createElement("hr")));
-            });
+        // And add a separator.
+        entries.append($(document.createElement("hr")));
+    });
 
     // And knock out that last separator.
     entries.find("hr").last().remove();
 }
 
-function finalizePosts()
-{
+function finalizePosts() {
     console.log("Found " + postData.length + " posts.");
 
     // Stop the long-loading timeout, if it's still waiting.
@@ -310,8 +291,7 @@ function finalizePosts()
     showAllPosts(base);
 }
 
-$(document).ready(function()
-{
+$(document).ready(function() {
     var widget = $(baseDiv);
 
     constructHtml(widget);
@@ -320,14 +300,11 @@ $(document).ready(function()
     widget.css("visibility", "visible");
 
     // So, where do we start?
-    if(!accountUrl)
-    {
+    if(!accountUrl) {
         showError("The accountUrl variable isn't defined or is empty; you'll need to look that up to use this widget.");
         console.error("accountUrl isn't defined or is empty; you'll need to look that up to use this.  It's right near the top of the masto_widget.js file.");
         return;
-    }
-    else
-    {
+    } else {
         // Quick!  To AJAX!  Start this whole thing in motion!
         fetchAccountData();
     }
