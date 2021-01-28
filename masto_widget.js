d19768f91600f78984d9539717019cb71aa20cad
Nicholas Killewald <captainspam@gmail.com>
2021-01-28 15:45:53 -0800

    Single-quotes EVERYWHERE!
    This is usually the better option.

--- masto_widget.js
+++ masto_widget.js
@@ -7,7 +7,7 @@
 */
 
 // The account URL is the baseline.  We can get to statuses relative to here.
-const accountUrl = "";
+const accountUrl = '';
 const statusesUrl = `${accountUrl}/statuses?limit=20`;
 
 // This is the base element in which we're putting this.  It will become a
@@ -17,17 +17,17 @@ var baseElem = undefined;
 var authorData = {};
 var postData = [];
 
-const loadingText = "Loading...";
+const loadingText = 'Loading...';
 var longLoadingTimeout;
-const longLoadingText = "Loading (in theory)...";
+const longLoadingText = 'Loading (in theory)...';
 const longLoadingDelay = 5000;
 
 function makeLink(href, text) {
     // Standard stuff that should go on every link.
-    const aElem = $(document.createElement("a"));
-    aElem.attr("rel", "nofollow noopener noreferrer");
+    const aElem = $(document.createElement('a'));
+    aElem.attr('rel', 'nofollow noopener noreferrer');
     if(href) {
-        aElem.attr("href", href);
+        aElem.attr('href', href);
     }
 
     if(text) {
@@ -45,17 +45,17 @@ function sanitizeHtmlToJQueryThingy(html) {
     // return it as text.
     if(!elem) {
         // Though, wrap it in a span, just to be polite.
-        const toReturn = $(document.createElement("span"));
+        const toReturn = $(document.createElement('span'));
         toReturn.text(html);
         return toReturn;
     }
 
     // Wrap it up in something so we can process it.
-    const wrapper = $(document.createElement("div"));
+    const wrapper = $(document.createElement('div'));
     wrapper.append(elem);
 
     // Next, remove all script elems inside.  None of THAT nonsense, now.
-    wrapper.find("script").remove();
+    wrapper.find('script').remove();
 
     // On each sub-element...
     wrapper.find('*').each(function(index, childElem) {
@@ -77,7 +77,7 @@ function sanitizeAttributesFromElement(elem) {
         // Remove any of the on* attributes, which might trigger JS.  Also,
         // remove any attribute that starts with "javascript:", as that's
         // HELLA suspicious.
-        if(attrName.startsWith("on") || attrVal.startsWith("javascript:")) {
+        if(attrName.startsWith('on') || attrVal.startsWith('javascript:')) {
             elem.removeAttribute(attrName);
         }
     });
@@ -93,23 +93,23 @@ function constructHtml() {
     baseElem.empty();
 
     // Make sure the base is a mw_container!
-    baseElem.addClass("mw_container");
+    baseElem.addClass('mw_container');
 
     const allOfTheHtml = $(`
-    <div class="mw_loading">${loadingText}</div>
-    <div class="mw_error"></div>
-    <div class="mw_mainblock">
-        <div class="mw_userblock">
-            <a rel="nofollow noopener noreferrer">
-                <div class="mw_avatar"></div>
+    <div class='mw_loading'>${loadingText}</div>
+    <div class='mw_error'></div>
+    <div class='mw_mainblock'>
+        <div class='mw_userblock'>
+            <a rel='nofollow noopener noreferrer'>
+                <div class='mw_avatar'></div>
             </a>
-            <div class="mw_userinfo">
-                <div class="mw_userdisplayname"></div>
-                <div class="mw_useratname"></div>
-                <div class="mw_summary"></div>
+            <div class='mw_userinfo'>
+                <div class='mw_userdisplayname'></div>
+                <div class='mw_useratname'></div>
+                <div class='mw_summary'></div>
             </div>
         </div>
-        <div class="mw_contentblock"></div>
+        <div class='mw_contentblock'></div>
     </div>`);
 
     // Also, let's add in a timeout to add the "(in theory)" text back in if
@@ -124,7 +124,7 @@ function fetchAccountData() {
     // we're just going to grab it now so we know what the user is.  What's
     // returned with individual statuses might be things like the original
     // author of a boosted status or whatnot.
-    $.get(accountUrl, "", function(data, textStatus, jqXHR) {
+    $.get(accountUrl, '', function(data, textStatus, jqXHR) {
         // Here comes author data!  And it's in nifty JSON format, too!
         authorData = extractAuthorDataFromJson(data);
 
@@ -134,7 +134,7 @@ function fetchAccountData() {
 
 function fetchStatuses() {
     // Status time!
-    $.get(statusesUrl, "", function(data, textStatus, jqXHR) {
+    $.get(statusesUrl, '', function(data, textStatus, jqXHR) {
         // Post data should just be an array of, well, post data.
         postData = data;
 
@@ -147,28 +147,28 @@ function extractAuthorDataFromJson(json) {
     const authorData = {};
 
     // Man, this is SO much nicer than guessing at what the RSS feed has...
-    authorData["displayName"] = json["display_name"];
-    authorData["uri"] = json["url"];
-    authorData["avatar"] = json["avatar"];
-    authorData["summary"] = json["note"];
+    authorData['displayName'] = json['display_name'];
+    authorData['uri'] = json['url'];
+    authorData['avatar'] = json['avatar'];
+    authorData['summary'] = json['note'];
 
     // TODO: Is this always true?  I don't see anything in Mastodon's output
     // that would imply otherwise.
-    authorData["summaryIsHtml"] = true;
+    authorData['summaryIsHtml'] = true;
 
     return authorData;
 }
 
 function showError(errorText) {
-    setMode(baseElem, "error");
-    const error = baseElem.find(".mw_error");
+    setMode(baseElem, 'error');
+    const error = baseElem.find('.mw_error');
     error.text(errorText);
 }
 
 function genericFetchError(data) {
     // Chances are the browser already dumped an error to console.log in this
     // case, so we don't need to do that here.
-    showError("There was some sort of problem reading your data.  If you're sure you typed it in right, maybe that server doesn't allow cross-domain Javascript widgets access to the feed (Mastodon instances in particular might deny access by default)?");
+    showError('There was some sort of problem reading your data.  If you\'re sure you typed it in right, maybe that server doesn\'t allow cross-domain Javascript widgets access to the feed (Mastodon instances in particular might deny access by default)?');
 }
 
 function setMode(modeString) {
@@ -177,40 +177,40 @@ function setMode(modeString) {
     // "loading"
     // "display"
     // "error"
-    if(modeString === "loading") {
-        baseElem.find(".mw_loading").toggle(true);
-        baseElem.find(".mw_mainblock").toggle(false);
-        baseElem.find(".mw_error").toggle(false);
-    } else if(modeString === "display") {
-        baseElem.find(".mw_loading").toggle(false);
-        baseElem.find(".mw_mainblock").toggle(true);
-        baseElem.find(".mw_error").toggle(false);
-    } else if(modeString === "error") {
-        baseElem.find(".mw_loading").toggle(false);
-        baseElem.find(".mw_mainblock").toggle(false);
-        baseElem.find(".mw_error").toggle(true);
+    if(modeString === 'loading') {
+        baseElem.find('.mw_loading').toggle(true);
+        baseElem.find('.mw_mainblock').toggle(false);
+        baseElem.find('.mw_error').toggle(false);
+    } else if(modeString === 'display') {
+        baseElem.find('.mw_loading').toggle(false);
+        baseElem.find('.mw_mainblock').toggle(true);
+        baseElem.find('.mw_error').toggle(false);
+    } else if(modeString === 'error') {
+        baseElem.find('.mw_loading').toggle(false);
+        baseElem.find('.mw_mainblock').toggle(false);
+        baseElem.find('.mw_error').toggle(true);
     }
 }
 
 function showAuthorData() {
-    baseElem.find(".mw_avatar").parent().attr("href", authorData["uri"]);
-    baseElem.find(".mw_avatar").css("background-image", "url(\"" + authorData["avatar"] + "\")");
+    baseElem.find('.mw_avatar').parent().attr('href', authorData['uri']);
+    baseElem.find('.mw_avatar').css('background-image', 'url("' + authorData['avatar'] + '")');
 
-    var aElem = makeLink(authorData["uri"], authorData["displayName"]);
-    baseElem.find(".mw_userdisplayname").append(aElem);
+    var aElem = makeLink(authorData['uri'], authorData['displayName']);
+    baseElem.find('.mw_userdisplayname').append(aElem);
 
-    const userAtName = baseElem.find(".mw_useratname");
-    aElem = makeLink(authorData["uri"], authorData["uri"]);
+    const userAtName = baseElem.find('.mw_useratname');
+    aElem = makeLink(authorData['uri'], authorData['uri']);
     userAtName.append(aElem);
-    if(authorData["summaryIsHtml"]) {
-        baseElem.find(".mw_summary").append(sanitizeHtmlToJQueryThingy(authorData["summary"]));
+    if(authorData['summaryIsHtml']) {
+        baseElem.find('.mw_summary').append(sanitizeHtmlToJQueryThingy(authorData['summary']));
     } else {
-        baseElem.find(".mw_summary").text(authorData["summary"]);
+        baseElem.find('.mw_summary').text(authorData['summary']);
     }
 }
 
 function showAllPosts() {
-    var entries = baseElem.find(".mw_contentblock");
+    var entries = baseElem.find('.mw_contentblock');
 
     // Later, we'll want to be able to update the content (i.e. adding more
     // entries after a timeout if more have been added at the source), but for
@@ -218,9 +218,9 @@ function showAllPosts() {
     entries.empty();
 
     $.each(postData, function(index, data) {
-        var entryElem = $(document.createElement("div"));
-        entryElem.addClass("mw_entry");
-        entryElem.attr("id", data["id"]);
+        var entryElem = $(document.createElement('div'));
+        entryElem.addClass('mw_entry');
+        entryElem.attr('id', data['id']);
 
         var curElem;
 
@@ -230,20 +230,20 @@ function showAllPosts() {
         // First, the date.  I'll do more conversion later, but for now, the
         // string we got for the date should be convertable to the local
         // timezone this way.  There's more stable ways to do this, I know.
-        var date = new Date(data["created_at"]);
-        curElem = $(document.createElement("div"));
-        curElem.addClass("mw_entry_date");
+        var date = new Date(data['created_at']);
+        curElem = $(document.createElement('div'));
+        curElem.addClass('mw_entry_date');
 
-        var aElem = makeLink(data["url"], date);
+        var aElem = makeLink(data['url'], date);
         curElem.append(aElem);
         entryElem.append(curElem);
 
         // TODO: This isn't how in-reply-to works, fix this.
-        if("in-reply-to" in data) {
-            curElem = $(document.createElement("div"));
-            curElem.addClass("mw_in_reply_to");
+        if('in-reply-to' in data) {
+            curElem = $(document.createElement('div'));
+            curElem.addClass('mw_in_reply_to');
 
-            aElem = makeLink(data["conversation"], "(part of a conversation)");
+            aElem = makeLink(data['conversation'], '(part of a conversation)');
             curElem.append(aElem);
             entryElem.append(curElem);
         }
@@ -253,20 +253,20 @@ function showAllPosts() {
         // dump that in to make it look right.  To that end, though, this needs
         // to be sanitized properly.  I would think Mastodon would sanitize
         // things on their side, but hey, never can be too sure, right?
-        curElem = $(document.createElement("div"));
-        curElem.addClass("mw_entry_content");
-        curElem.append(sanitizeHtmlToJQueryThingy(data["content"]));
+        curElem = $(document.createElement('div'));
+        curElem.addClass('mw_entry_content');
+        curElem.append(sanitizeHtmlToJQueryThingy(data['content']));
 
         // Unlike the RSS version, it looks like Mastodon already takes care of
-        // rel and target="_blank" stuff.  That's handy.
+        // rel and target='_blank' stuff.  That's handy.
 
         entryElem.append(curElem);
 
         // If we've got any media to attach, attach it.
-        const media = data["media_attachments"];
+        const media = data['media_attachments'];
         if(media && media.length > 0) {
-            const mediaContainer = $(document.createElement("div"));
-            mediaContainer.addClass("mw_media_container");
+            const mediaContainer = $(document.createElement('div'));
+            mediaContainer.addClass('mw_media_container');
 
             // Keep track of how much media we've ACTUALLY added.  If all the
             // attachments are things we can't handle, don't bother adding the
@@ -276,16 +276,16 @@ function showAllPosts() {
             $.each(media, function(mediaIndex, mediaData) {
                 // TODO: Other media types?  We're just ignoring anything that
                 // isn't an image for now.
-                if(mediaData["type"] === "image") {
-                    const mediaElem = $(document.createElement("div"));
-                    mediaElem.addClass("mw_media_item");
+                if(mediaData['type'] === 'image') {
+                    const mediaElem = $(document.createElement('div'));
+                    mediaElem.addClass('mw_media_item');
 
-                    const aElem = makeLink(mediaData["url"]);
+                    const aElem = makeLink(mediaData['url']);
 
-                    const imgElem = $(document.createElement("img"));
-                    imgElem.attr("src", mediaData["preview_url"]);
-                    imgElem.attr("title", mediaData["description"]);
-                    imgElem.attr("alt", mediaData["description"]);
+                    const imgElem = $(document.createElement('img'));
+                    imgElem.attr('src', mediaData['preview_url']);
+                    imgElem.attr('title', mediaData['description']);
+                    imgElem.attr('alt', mediaData['description']);
 
                     aElem.append(imgElem);
                     mediaElem.append(aElem);
@@ -293,7 +293,7 @@ function showAllPosts() {
 
                     mediaAdded++;
                 } else {
-                    console.warn(`Don't know how to handle media of type '${mediaData["type"]}', ignoring...`);
+                    console.warn(`Don't know how to handle media of type '${mediaData['type']}', ignoring...`);
                 }
             });
 
@@ -306,36 +306,36 @@ function showAllPosts() {
         entries.append(entryElem);
 
         // And add a separator.
-        entries.append($(document.createElement("hr")));
+        entries.append($(document.createElement('hr')));
     });
 
     // And knock out that last separator.
-    entries.find("hr").last().remove();
+    entries.find('hr').last().remove();
 }
 
 function finalizePosts() {
-    console.log("Found " + postData.length + " posts.");
+    console.log('Found ' + postData.length + ' posts.');
 
     // Stop the long-loading timeout, if it's still waiting.
     clearTimeout(longLoadingTimeout);
 
-    setMode("display");
+    setMode('display');
     showAuthorData();
     showAllPosts();
 }
 
 $(document).ready(function() {
     // As this is an iframe, we want the base to be the overall body element.
-    baseElem = $("body");
+    baseElem = $('body');
     constructHtml();
-    setMode("loading");
+    setMode('loading');
 
-    baseElem.css("visibility", "visible");
+    baseElem.css('visibility', 'visible');
 
     // So, where do we start?
     if(!accountUrl) {
-        showError("The accountUrl variable isn't defined or is empty; you'll need to look that up to use this widget.");
-        console.error("accountUrl isn't defined or is empty; you'll need to look that up to use this.  It's right near the top of the masto_widget.js file.");
+        showError('The accountUrl variable isn\'t defined or is empty; you\'ll need to look that up to use this widget.');
+        console.error('accountUrl isn\'t defined or is empty; you\'ll need to look that up to use this.  It\'s right near the top of the masto_widget.js file.');
         return;
     } else {
         // Quick!  To AJAX!  Start this whole thing in motion!
